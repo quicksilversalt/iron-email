3547b932908ee3bcdd15d66858f2b484d7ae4bf4
Danny Hobart <dh@dannyhobart.com>
2015-05-05 01:00:41 -0400

    messy code but this works to keep an open idle connection on two separate inboxes at the same time

--- mailProcessor.js
+++ mailProcessor.js
@@ -5,11 +5,13 @@ var notifier = require('mail-notifier')
 var filename = __dirname + "/config.json";
 var config = JSON.parse (fs.readFileSync(filename,'utf8'));
 
-var imap = {
+var userConfig = {
         user: config.username,
         password: config.password,
         host: config.imap.host,
         port: config.imap.port,
+        markSeen: false,
+        //box: '[Gmail]/Spam',
          tls: true,// use secure connection
         tlsOptions: { rejectUnauthorized: false }
 };
@@ -17,7 +19,7 @@ var imap = {
 //---------------------------------------------------------------------------------------------------------
 // Mail Processor  mail receive event
 //---------------------------------------------------------------------------------------------------------
-var mailProcessor  = notifier(imap).on('mail', function (mail) {
+var mailProcessor  = notifier(userConfig).on('mail', function (mail) {
 
     //Build a custom json email message from "mail" object.
     //mail has lot of other properties, please use it as per your requirement.
@@ -32,17 +34,21 @@ var mailProcessor  = notifier(imap).on('mail', function (mail) {
     //[Optional]Save the email message to Mongo
    //db.save(JSON.stringify(msg));
    console.log("mail detected");
-   console.log(JSON.stringify(msg));
+   console.log(mail.from[0].address);
+   console.log("subject is " + JSON.stringify(msg.subject));
+   //if(JSON.stringify(msg.subject).indexOf("move to spam") != -1){
+   // console.log("should move this to spam!!!!!!!!!!!!!!!!!!!!!");
+   //}
 
     //Check for attachments, if exists extract each attachment and save it to the file system ["uploads" folder] 
-    if (mail.attachments) {
-        mail.attachments.forEach(function (attachment) {
-            fs.writeFile( __dirname + "/uploads/" + attachment.generatedFileName, attachment.content, 'base64', function(err) {
-                   if (err!=null)
-                        console.log("Error = " + err);
-                });
-        });
-    }
+    //if (mail.attachments) {
+    //    mail.attachments.forEach(function (attachment) {
+    //        fs.writeFile( __dirname + "/uploads/" + attachment.generatedFileName, attachment.content, 'base64', function(err) {
+    //               if (err!=null)
+    //                    console.log("Error = " + err);
+    //            });
+    //    });
+    //}
     //[Optional]Save the email message to file system as .txt/.json
     /*fs.writeFile("/tmp/test/email_" + mail.messageId + '.txt', JSON.stringify(msg), function (err) {
         if (err) {
--- scanAndMove.js
+++ scanAndMove.js
@@ -1,11 +1,13 @@
 var notifier = require('mail-notifier')
 , fs = require('fs')
 , Imap = require('imap')
+, Imap2 = require('imap')
 , stream = require('stream')
 , MailParser = require('mailparser').MailParser;
 
 var filename = __dirname + "/config.json";
 var config = JSON.parse (fs.readFileSync(filename,'utf8'));
+var topUID, topUID2;
 
 
 var usrInfo = {
@@ -19,15 +21,44 @@ var usrInfo = {
         tlsOptions: { rejectUnauthorized: false }
 };
 
-var imap = new Imap(usrInfo);
+var usr1 = {
+	user: "elephantventurestester@gmail.com",
+    password: "ereIamJH",
+    host: "imap.gmail.com",
+    port: 993,
+    markSeen: false,
+    box: 'INBOX',
+     tls: true,// use secure connection
+    tlsOptions: { rejectUnauthorized: false }
+}
+
+var usr2 = {
+	user: "evdhtester@gmail.com",
+    password: "#Abc123!",
+    host: "imap.gmail.com",
+    port: 993,
+    markSeen: false,
+    box: '[Gmail]/Spam',
+     tls: true,// use secure connection
+    tlsOptions: { rejectUnauthorized: false }
+}
+
+var imap = new Imap(usr2);
+var imap2 = new Imap2(usr1);
+
 
 imap.once('ready', function() {
+	console.log("checking imap");
     openImapMailbox('INBOX');
+});
+imap2.once('ready', function() {
+	console.log("checking imap2");
+    openImapMailbox2('INBOX');
 });
 
 function openImapMailbox(mailBox){
 	imap.openBox(mailBox, false, function(err, box) {
-		console.log(box);
+		//console.log(box);
         searchForNewEmail(box); // initial search/process of data
         imap.on('mail', function (id) {
             searchForNewEmail(box);
@@ -35,8 +66,18 @@ function openImapMailbox(mailBox){
     });
 }
 
+function openImapMailbox2(mailBox){
+	imap2.openBox(mailBox, false, function(err, box) {
+		//console.log(box);
+        searchForNewEmail2(box); // initial search/process of data
+        imap2.on('mail', function (id) {
+            searchForNewEmail2(box);
+        });
+    });
+}
+
 function searchForNewEmail(box) {
-    imap.search(['ALL'], function(err, results) {
+    imap.search([box.messages.total + ':*'], function(err, results) {
     	if (err) {
             console.log(err);
         }
@@ -44,7 +85,7 @@ function searchForNewEmail(box) {
             console.log('no new mail in INBOX');
             return;
         }
-        var f = imap.fetch(results, { bodies: 'HEADER.FIELDS (TO FROM SUBJECT)', struct: true });
+        var f = imap.seq.fetch(results, { bodies: 'HEADER.FIELDS (TO FROM SUBJECT)', struct: true });
         console.log("============================================");
         console.log("");
         console.log("SEARCHING " + box.name);
@@ -54,19 +95,73 @@ function searchForNewEmail(box) {
         	var mp = new MailParser();
         	var shouldMove, uid;
         	mp.once('end', function (mail) {
+        		//console.log(mail.from);
                 console.log(mail.subject);
                 console.log("^^^^^");
                 if(mail.subject !== undefined && mail.subject.indexOf("move to spam") != -1){
                 	shouldMove = true;
                 	if(uid !== undefined){
-                		moveMail(uid,"[Gmail]/Spam");
+                		console.log("would move");
+                		//moveMail(uid,"[Gmail]/Spam");
                 	}
                 }
             });
             msg.once('attributes', function(attrs) {
                 uid = attrs.uid;
                 if (shouldMove === true){
-                  moveMail(uid,"[Gmail]/Spam");
+                	console.log("would move");
+                  //moveMail(uid,"[Gmail]/Spam");
+                }
+            });
+            msg.on('body', function(stream, info) {
+            	stream.pipe(mp);
+                var buffer = '';
+                stream.on('data', function(chunk) {
+                  //buffer += chunk.toString('utf8');
+                });
+                stream.once('end', function() {
+                    // do something with 'buffer'
+                });
+            });
+        });
+    });
+}
+
+function searchForNewEmail2(box) {
+    imap2.search([box.messages.total + ':*'], function(err, results) {
+    	if (err) {
+            console.log(err);
+        }
+        if (!results || results.length === 0) {
+            console.log('no new mail in INBOX');
+            return;
+        }
+        var f = imap2.fetch(results, { bodies: 'HEADER.FIELDS (TO FROM SUBJECT)', struct: true });
+        console.log("============================================");
+        console.log("");
+        console.log("SEARCHING " + box.name);
+        console.log("");
+        console.log("============================================");
+        f.on('message', function(msg, seqno) {
+        	var mp = new MailParser();
+        	var shouldMove, uid;
+        	mp.once('end', function (mail) {
+        		//console.log(mail.from);
+                console.log(mail.subject);
+                console.log("^^^^^");
+                if(mail.subject !== undefined && mail.subject.indexOf("move to spam") != -1){
+                	shouldMove = true;
+                	if(uid !== undefined){
+                		console.log("would move");
+                		//moveMail(uid,"[Gmail]/Spam");
+                	}
+                }
+            });
+            msg.once('attributes', function(attrs) {
+                uid = attrs.uid;
+                if (shouldMove === true){
+                	console.log("would move");
+                  //moveMail(uid,"[Gmail]/Spam");
                 }
             });
             msg.on('body', function(stream, info) {
@@ -87,11 +182,12 @@ function moveMail (uid, box) {
     console.log("====== move mail with: ");
     console.log(uid);
     console.log(box);
-    imap.move(uid, box, function(err, code){
-        if(err){
-            console.log(err);
-        }
-    })
+    //imap.move(uid, box, function(err, code){
+    //    if(err){
+    //        console.log(err);
+    //    }
+    //})
 }
 
 imap.connect(); // connect to imap
+imap2.connect(); // connect to imap
